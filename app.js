b4d60a3bb8f61b6a9a0fc8a68f05f916a303a228
Jerry D'Antonio <stumpjumper@gmail.com>
2017-08-04 23:53:11 -0400

    Rudimentary spell checker.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+require('dotenv').config()
+
+const restify = require('restify');
+const builder = require('botbuilder');
+
+const dictionary = require('dictionary-en-us');
+const nspell = require('nspell');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -9,8 +14,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users 
@@ -18,5 +23,22 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+
+  dictionary((err, dict) => {
+    if (err) {
+      session.send("Error: %s", err.message);
+    }
+
+    var spell = nspell(dict);
+    var suggestions = spell.suggest(session.message.text);
+    // var correct = spell.correct(session.message.text); // boolean
+
+    if (suggestions.length === 0) {
+      session.send("Correct: %s", session.message.text);
+    } else {
+      session.send("Suggestions: %s", suggestions.join(', '));
+    }
+  });
+
+  // session.send("You said: %s", session.message.text);
 });
